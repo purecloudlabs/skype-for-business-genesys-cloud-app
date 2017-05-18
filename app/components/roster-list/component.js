fa4ba11264b708be6bbe1c621b114ecc9df20131
Eric Lifka <eric.lifka@inin.com>
2017-05-18 15:46:13 -0400

    limited roster data showing up

--- app/components/roster-list/component.js
+++ app/components/roster-list/component.js
@@ -11,7 +11,12 @@ export default Component.extend({
 
     skype: inject.service(),
 
-    groups: [],
+    init() {
+        this._super(...arguments);
+
+        this.set('groups', []);
+        window.GROUPS = this.get('groups');
+    },
 
     didInsertElement() {
         this._super(...arguments);
@@ -26,13 +31,54 @@ export default Component.extend({
     },
 
     fetchData() {
-        const skype = this.get('skype');
-        skype.getAllGroups().then(groups => {
-            this.get('groups').addObjects(groups);
-        });
+        // const skype = this.get('skype');
+        //
+        // console.log('roster group search started');
+        // skype.getAllGroups().then(groups => {
+        //     console.log('roster groups: ', groups, groups.map(g => mapSkypeToPojo(g)));
+        //     setTimeout(() => {
+        //         console.log('roster groups delayed: ', groups, groups.map(g => mapSkypeToPojo(g)));
+        //     }, 1000);
+        //
+        //     window.GROUPS = groups;
+        //     // this.get('groups').addObjects(groups);
+        // });
+        //
+        // console.log('roster person search started');
+        // skype.get('application').personsAndGroupsManager.all.persons
+        //     .get().then(function (contacts) {
+        //         console.log('roster person search results: ', contacts, contacts.map(c => c.displayName()));
+        //
+        //     }, function (error) {
+        //         console.log('roster person search error: ', error);
+        //     });
     },
 
-    addPerson() {},
-    addConversation() {},
-    addGroup() {}
+    addPerson(person) {
+        person.displayName.get().then(name => {
+            console.log('ROSTER: addPerson', name, person.id());
+        })
+    },
+    addConversation(conversation) {
+    },
+    addGroup(group) {
+        let groupModel = Ember.Object.create();
+        this.get('groups').pushObject(groupModel);
+
+        group.id.get().then(() => {
+            groupModel.set('id', group.id());
+            groupModel.set('name', group.name());
+            groupModel.set('persons', []);
+
+            group.persons().forEach(person => {
+                let personModel = Ember.Object.create();
+                groupModel.get('persons').pushObject(personModel);
+
+                person.displayName.get().then(() => {
+                    personModel.set('id', person.id());
+                    personModel.set('displayName', person.displayName());
+                });
+            });
+        });
+    }
 });
