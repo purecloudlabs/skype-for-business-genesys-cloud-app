5a3aefb4dccbfeca5edf01005269c0a43c41c7be
Keri Lawrence <keri.lawrence@genesys.com>
2017-09-14 10:55:31 -0400

    switch to non-ui api

--- app/services/skype.js
+++ app/services/skype.js
@@ -12,7 +12,6 @@ const {
 
 const config = {
     apiKey: 'a42fcebd-5b43-4b89-a065-74450fb91255', // SDK
-    apiKeyCC: '9c967f6b-a846-4df2-b43d-5167e47d81e1' // SDK+UI
 };
 
 const redirectUri =
@@ -56,13 +55,10 @@ export default Service.extend(Evented, {
         this.promise = deferred.promise;
 
         window.Skype.initialize({
-            apiKey: config.apiKeyCC,
-            supportsAudio: true,
-            supportsVideo: true,
-            convLogSettings: true
+            apiKey: config.apiKey,
         }, api => {
             this.api = api;
-            this.application = api.UIApplicationInstance;
+            this.application =  new api.application();
 
             deferred.resolve();
         }, error => {
