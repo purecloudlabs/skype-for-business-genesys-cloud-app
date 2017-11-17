546d1e567326fce38746e702213c7c1f0d80f84e
Omar Estrella <omar.estrella@inin.com>
2017-11-17 15:52:17 -0500

    Adjust presence indicators

--- app/components/presence-indicator/component.js
+++ app/components/presence-indicator/component.js
@@ -1,10 +1,25 @@
 import Ember from 'ember';
 
 const {
+    computed,
     Component
 } = Ember;
 
+const ICON_MAPPING = {
+    available: 'ion-checkmark-round',
+    away: 'ion-clock',
+    offline: 'ion-close-round'
+}
+
 export default Component.extend({
     tagName: 'span',
-    classNames: ['presence-indicator']
+    classNames: ['presence-indicator'],
+    classNameBindings: ['user.presenceClass', 'iconClass'],
+
+    user: null,
+
+    iconClass: computed('user.presence', function () {
+        const presence = this.get('user.presence').toLowerCase();
+        return ICON_MAPPING[presence];
+    })
 });
