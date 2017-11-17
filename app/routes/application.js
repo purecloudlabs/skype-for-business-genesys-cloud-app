abb4adf8e1554de554b6372014ef560d39988593
Joseph.Barnes <joseph.barnes@inin.com>
2017-11-17 16:40:31 -0500

    include localforage; add auth for purecloud apps at app start

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,3 +1,4 @@
+/* global localforage */
 import Ember from 'ember';
 
 const {
@@ -10,12 +11,33 @@ export default Route.extend({
     auth: inject.service(),
 
     beforeModel(transition) {
-        this.get('auth').silentLogin().then(() => {
-            let target = transition.targetName;
-            this.transitionTo(target);
-        }).catch(error => {
-            Logger.error('Error logging in silently', error);
-            this.transitionTo('index');
-        })
+        localforage.config({
+            name: 'pureSkype',
+            version: 1.0,
+            storeName: 'forage',
+            description: 'Storing local preferences for the Skype for Business integration app'
+        });
+
+        let ref = window.location.href;
+        let tokenIndex = ref.indexOf('access_token');
+        localforage.getItem('forage.token.purecloud').then((cookie) => {
+            if (tokenIndex != -1) {
+                let token = ref.substring(tokenIndex + 13, ref.indexOf('&'));
+                this.get('auth').set('purecloudAccessToken', token);
+                this.get('auth').setTokenCookie(token, 'purecloud');
+            } else if (cookie) {
+                this.get('auth').set('purecloudAccessToken', cookie);
+            } else {
+                this.get('auth').purecloudAuth();
+            }
+        }).then(() => {
+            this.get('auth').silentLogin().then(() => {
+                let target = transition.targetName;
+                this.transitionTo(target);
+            }).catch(error => {
+                Logger.error('Error logging in silently', error);
+                this.transitionTo('index');
+            })
+        });
     }
 });
