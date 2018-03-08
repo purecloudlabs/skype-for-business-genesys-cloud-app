efd16ac1375e9e8064d564a593ec46f42d415dbb
Omar Estrella <omar.estrella@inin.com>
2018-03-08 11:07:22 -0500

    Use ember-chromium for better test runs

--- testem.js
+++ testem.js
@@ -1,12 +1,3 @@
 /* eslint-env node */
-module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
-  ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
-};
+module.exports = require('ember-chromium').getTestemConfig();
+
