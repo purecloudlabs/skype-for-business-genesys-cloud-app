4282567cea0935347518973c5fcf28b55ac33675
Omar Estrella <omarestrella@bitcreative.net>
2018-12-05 14:22:30 -0500

    Update app sdk (#49)
    * Update app sdk
    
    * Change library import

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -38,7 +38,7 @@ module.exports = function (defaults) {
     if (app.env === 'test') {
         console.info('Not including App SDK during testing');
     } else {
-        app.import('node_modules/@purecloud/purecloud-client-app-sdk/dist/purecloud-client-app-sdk.js');
+        app.import('node_modules/purecloud-client-app-sdk/dist/purecloud-client-app-sdk.js');
     }
 
     app.import('node_modules/localforage/dist/localforage.js');
